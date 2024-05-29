5d8ff528a84fb2dbb572d8aea6416cd1ab9aacab
Iza Cabral <izabelecabral@hotmail.com>
2024-05-29 18:23:44 -0300

    hotfix: just to handle arguments as subject

---
+++ defines.hpp
@@ -0,0 +1,11 @@
+#ifndef DEFINES_HPP
+# define DEFINES_HPP
+
+#define BLUE	"\033[34m"
+#define RED		"\033[31m"
+#define YLW		"\033[33m"
+#define ORA		"\033[38;2;255;153;0m"
+#define GRN		"\033[32m"
+#define RST		"\033[0m"
+
+#endif
